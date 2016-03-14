b69fb90bb2a4ab25cd20b3f4ee284ebb110f7c4f
Jacky Boen <jacky.boen@bartleboglehegarty.com>
2016-03-15 04:55:47 +0800

    Update
    Signed-off-by: Jacky Boen <jacky.boen@bartleboglehegarty.com>

--- UV.cpp
+++ UV.cpp
@@ -6,7 +6,6 @@ const float UV::minValue = 0;
 const float UV::maxValue = 15;
 
 static const int UVOUT = A1;
-static const int REF_3V3 = A2;
 
 UV::UV() :
 	value(0)
@@ -15,11 +14,10 @@ UV::UV() :
 
 void UV::init() {
 	pinMode(UVOUT, INPUT);
-	pinMode(REF_3V3, INPUT);
 }
 
 void UV::update() {
-	int uvLevel = map(analogRead(UVOUT), 221, 1024, 0, 1024);
+	int uvLevel = map(analogRead(UVOUT), 200, 1024, 0, 1024);
 	delay(10);
 	const int refLevel = 1024 * 3.3 / 5;
 	delay(10);
@@ -28,8 +26,8 @@ void UV::update() {
 		uvLevel = 0;
 
 	const float outputVoltage = 3.3 / refLevel * uvLevel;
-	//Serial.print(uvLevel);
-	//Serial.print(" ");
-	//Serial.println(refLevel);
+	Serial.print(uvLevel);
+	Serial.print(" ");
+	Serial.println(refLevel);
 	value = mapfloat(outputVoltage, 0.0, 3.3, minValue, maxValue); // Convert the voltage to a UV intensity level
 }
